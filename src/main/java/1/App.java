c787cabd869f53d59fcbf3d85ef1c25dec95e13d
Jan Starka <xstarka@nymfe15.fi.muni.cz>
2015-09-30 14:29:32 +0200

    first commit

---
+++ src/main/java/1/App.java
@@ -0,0 +1,13 @@
+package net.fimuni.xstark;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello PA165!" );
+    }
+}
